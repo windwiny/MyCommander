e760d98b8c0915763c9d709b92a1bc39e464375e
windwiny <windwiny.ubt@gmail.com>
2014-01-19 23:51:10 +0800

    3

--- MyCommand.rb
+++ MyCommand.rb
@@ -20,16 +20,23 @@ end
 $root = TkRoot.new do |root|
   $main_frame = Ttk::Frame.new(root) do |main_frame|
     Ttk::Frame.new(main_frame) do |frame|
+      xscr = Ttk::Scrollbar.new(frame, :orient=>"horizontal").grid(:row=>1, :column=>0, :sticky=>'we', :in=>frame)
+      yscr = Ttk::Scrollbar.new(frame, :orient=>"vertical").grid(:row=>0, :column=>1, :sticky=>'ns', :in=>frame)
       Tk::Text.new() { |t|
         t.value = ''
-      }.pack('expand'=>'yes', 'fill'=>'both', 'in'=>frame)
+        t.wrap 'none'
+        t.width 82
+        t.height 32
+        t.xscrollbar xscr
+        t.yscrollbar yscr
+      }.grid(:row=>0, :column=>0, :sticky=>"nsew", 'in'=>frame)
+      frame.grid_rowconfigure 0, :weight=>1
+      frame.grid_columnconfigure 0, :weight=>1
     end.pack('expand'=>'yes', 'fill'=>'both', 'padx'=>'1m')
 
     Ttk::Frame.new(main_frame) do |frame|
       Ttk::Button.new(frame) {
         text 'bsf 3'
-        # width 120
-        # height 30
       }.pack('side'=>'left', 'expand'=>'yes', 'fill'=>'x')
     end.pack('fill'=>'x', 'pady'=>'1m', 'padx'=>'1m')
 
@@ -73,7 +80,7 @@ $root = TkRoot.new do |root|
       }.pack('side'=>'left', 'expand'=>'yes', 'fill'=>'x')
     end.pack('fill'=>'x', 'pady'=>'1m', 'padx'=>'1m')
 
-  end.pack('expand'=>'yes', 'fill' => 'both')
+  end.pack('expand'=>'yes', 'fill'=>'both')
 end
 
 Tk.mainloop
